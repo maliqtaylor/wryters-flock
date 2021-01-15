e90fa434834a0ff68773ff1bcb0e3ff55d269cf4
maliqtaylor <maliq.taylor@icloud.com>
2021-01-15 02:31:54 -0500

    stub up feedback controllers

--- models/entry.js
+++ models/entry.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const feedbackSchema = new Schema ({
-    author: [{type: Schema.Types.ObjectId, ref: "User"}],
-    content: String,
-    liked: Boolean,  //Revise if UI unintuitive
+const likeSchema = new Schema ({
+    liker: {type: Schema.Types.ObjectId, ref: "User"},
+    liked: {type:Boolean, default: null}
 }, 
 {timestamps: true}
+)
+
+const commentSchema = new Schema({
+  commentor: { type: Schema.Types.ObjectId, ref: "User" },
+  content: String,
+  //add replies later
+},
+  { timestamps: true }
+)
+
+const ratingSchema = new Schema({
+  rated_by: { type: Schema.Types.ObjectId, ref: "User" },
+  rating: Number,
+},
+  { timestamps: true }
 )
 
 const entrySchema = new Schema ({
@@ -15,9 +29,11 @@ const entrySchema = new Schema ({
     private: {type: Boolean, required: true},
     classification: {type: String,  enum:["Poem", "Short Story", "Blog", "Lyrics", "Novel", "Essay", "Speech", "Fan-Fic", "Journal", "Play"]},
     genre: { type: String, enum: ["Fantasy", "Mystery", "Drama", "Comedy", "AutoBiography", "Non-Fiction", "Fiction", "Sci-Fi", "Horror", "Romance", "Historical", "Myth"]},
-    author: [{type: Schema.Types.ObjectId, ref: "User"}],
+    author: {type: Schema.Types.ObjectId, ref: "User"},
     content: String,
-    feedback: [feedbackSchema]
+    comments: [commentSchema],
+    likes: [likeSchema],
+    rating: [ratingSchema]
 },
 {timestamps: true}
 )
