43a18a0d9326f560a1e1852aa6f0ebb454dccc3e
maliqtaylor <maliq.taylor@icloud.com>
2021-01-15 01:31:01 -0500

    add entries router to server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require('./config/database');
 
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
+const entriesRouter = require('./routes/entries');
 
 const cors = require('cors')
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
+app.use('/api/entries', entriesRouter);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
