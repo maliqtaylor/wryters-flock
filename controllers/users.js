8e00ee368de1668bd8b8cd0eba28441dd5c2f01d
Looloo414 <alexafaulkison@gmail.com>
2021-01-14 21:54:32 -0700

    User Cntrl and Route

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,20 @@
 const User = require("../models/user");
 
 module.exports = {
-  index,
+  update,
+  profile
 };
 
-function index(req, res) {
-  console.log("req.user", req.user);
-  User.find({}).then((users) => res.json(users));
+function profile(req, res) {
+  User.findOne(req.user._id)
+  .then(userData => {
+    res.json(userData)})
+  .catch(err => {res.json(err)})
 }
+
+function update(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(user => {res.json(user)})
+  .catch(err => {res.json(err)})
+}
+
