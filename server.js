0ec71c70dba4b578876be8980f32c0085f668b19
Looloo414 <alexafaulkison@gmail.com>
2021-01-14 22:54:20 -0700

    Fixed Icon Error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 
