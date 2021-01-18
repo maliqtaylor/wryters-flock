355738cbef51a940592a909ee9a760e2223b0d25
abonilla1 <amariabrown015@gmail.com>
2021-01-18 11:05:59 -0600

    refacotred routes to protect draft

--- src/pages/Draft/CreateDraft.jsx
+++ src/pages/Draft/CreateDraft.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import Document from "../../components/Document/Document"
 import EntryData from "../../components/EntryData/EntryData"
+import { useHistory } from "react-router-dom"
 import { useContent } from "../../components/hooks/useContent"
 
 const CreateDraft = (props) => {
-
+  const history = useHistory()
   console.log(props.history.location.state)
 
   const [content, setContent] = useContent({
     content: ""
   })
 
-  return (
-    <>
-      <Document
-        value={content.content}
-        onChange={setContent}
-      />
-      <EntryData
-        draftId={props.history.location.state.draftId}
-      />
-    </>
-  )
+  if (props.history.location.state ) {               
+    return (
+      <>
+        <Document
+          value={content.content}
+          onChange={setContent}
+        />
+        <EntryData
+          draftId={props.history.location.state.draftId}
+        />
+      </>
+    )
+  } else { 
+    history.push("/profile")
+    return null
+  }
 }
 
 export default CreateDraft
