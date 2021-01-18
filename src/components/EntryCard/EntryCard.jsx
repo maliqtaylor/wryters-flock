a6f106d63623b89e9487d02203614596b73fcc7b
maliqtaylor <maliq.taylor@icloud.com>
2021-01-18 14:43:53 -0500

    fix content undefined err

--- src/components/EntryCard/EntryCard.jsx
+++ src/components/EntryCard/EntryCard.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from 'semantic-ui-react'
 
-function EntryCard ({ user, entry }) {
-    console.log(entry)
-    return(
-        <>
-            <Container>
-                <p>
-                  hi
-                </p>
-            </Container>
-        </>
-    )
+function EntryCard({ user, entry }) {
+  console.log(entry)
+  return (
+    <>
+      <Container>
+        {entry.content ?
+          <p>{entry.content.content}</p>
+          :
+          <p>NA</p>}
+      </Container>
+    </>
+  )
 }
 
 export default EntryCard
